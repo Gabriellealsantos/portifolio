b8a94e9e38e82545525988ae31e3d02754b7784e
gabriel <gabriel.lealsantos@hotmail.com>
2025-02-20 13:09:15 -0300

    styles: projeto clima

--- src/components/TextCard/index.tsx
+++ src/components/TextCard/index.tsx
@@ -5,20 +5,23 @@ type TextCardProps = {
     title: string;
     functionLabel: string;
     texts: string[]; // Mais flexível para qualquer número de textos
+    git: string;
 };
 
-export const TextCard = ({ title, functionLabel, texts }: TextCardProps) => (
+export const TextCard = ({ title, functionLabel, texts, git }: TextCardProps) => (
     <BaseCard className="card-project" shadowDirection="left">
-        <div className="header-card-container">
-            <div className="style-bar-container pf-mt20">
-                <div className="rectangular"></div>
-                <h1>{title} 🔗 <p>{functionLabel}</p></h1>
+        <a href={git} target="_blank" rel="noopener noreferrer">
+            <div className="header-card-container">
+                <div className="style-bar-container pf-mt20">
+                    <div className="rectangular"></div>
+                    <h1>{title} 🔗 <p>{functionLabel}</p></h1>
+                </div>
             </div>
-        </div>
-        <div className="container-project-p">
-            {texts.map((text, index) => (
-                <p key={index}>{text}</p>
-            ))}
-        </div>
+            <div className="container-project-p">
+                {texts.map((text, index) => (
+                    <p key={index}>{text}</p>
+                ))}
+            </div>
+        </a>
     </BaseCard>
 );
--- src/routes/PortifolioHome/Project/index.tsx
+++ src/routes/PortifolioHome/Project/index.tsx
@@ -4,6 +4,7 @@ import gitProjectImg from '../../../assets/gitproject.png';
 import lolProjectImg from '../../../assets/lol.png';
 import portifolioImg from '../../../assets/portifolio.png';
 import deliveryImg from '../../../assets/Delivery.png';
+import climaImg from '../../../assets/clima.png';
 import FooterPf from '../../../components/FooterPf';
 import { ImageCard } from '../../../components/ImageCard';
 import { TextCard } from '../../../components/TextCard';
@@ -13,60 +14,78 @@ export default function Project() {
     const projects = [
         {
             title: 'GABRIEL LEAL | Portfólio',
-            functionLabel: 'Função',
+            functionLabel: 'E-commerce',
             texts: [
                 'Esse projeto reúne minhas habilidades, competências, qualificações e experiências profissionais.',
                 'É o projeto mais profissional, descritivo e organizado que já construí do total zero e sem nenhuma ajuda.',
                 'Continuarei atualizando-o com novos projetos cada vez mais complexos.'
             ],
             image: dsCommerceImg,
-            link: 'https://www.linkedin.com/posts/gabriel-leal-santos_conclu%C3%AD-meu-curso-na-devsuperior-com-o-projeto-activity-7221602574993395712-HmIa'
+            link: 'https://www.linkedin.com/posts/gabriel-leal-santos_conclu%C3%AD-meu-curso-na-devsuperior-com-o-projeto-activity-7221602574993395712-HmIa',
+            github: 'https://github.com/Gabriellealsantos/-DSCommerce'
         },
         {
             title: 'GABRIEL LEAL | Portfólio',
-            functionLabel: 'Função',
+            functionLabel: 'Curriculo',
             texts: [
                 'Esse projeto reúne minhas habilidades, competências, qualificações e experiências profissionais.',
                 'Projeto construído com autonomia completa desde a concepção.',
                 'Atualizações constantes com novos desafios profissionais.'
             ],
             image: portifolioImg,
-            link: 'https://www.linkedin.com/posts/gabriel-leal-santos_bom-dia-galera-estou-animado-em-compartilhar-activity-7126550439151435776-YE9_'
+            link: 'https://www.linkedin.com/posts/gabriel-leal-santos_bom-dia-galera-estou-animado-em-compartilhar-activity-7126550439151435776-YE9_',
+            github: 'https://github.com/Gabriellealsantos/portifolio'
         },
         {
-            "title": "Profile Search | GitHub-API",
-            "functionLabel": "Função",
-            "texts": [
-                "Aplicação interativa para busca e exibição de perfis do GitHub em tempo real.",
-                "Integração eficiente com a API do GitHub, permitindo acesso a repositórios, seguidores e outras informações.",
-                "Desenvolvido com foco em consumo de APIs externas e arquitetura escalável para futuras melhorias."
+            title: 'Profile Search | GitHub-API',
+            functionLabel: 'Busca de Perfis',
+            texts: [
+                'Aplicação interativa para busca e exibição de perfis do GitHub em tempo real.',
+                'Integração eficiente com a API do GitHub, permitindo acesso a repositórios, seguidores e outras informações.',
+                'Desenvolvido com foco em consumo de APIs externas e arquitetura escalável para futuras melhorias.'
             ],
-            "image": gitProjectImg,
-            "link": "https://www.linkedin.com/posts/gabriel-leal-santos_bom-dia-pessoal-estou-empolgado-em-compartilhar-activity-7206303763706339328-ylh9"
+            image: gitProjectImg,
+            link: 'https://www.linkedin.com/posts/gabriel-leal-santos_bom-dia-pessoal-estou-empolgado-em-compartilhar-activity-7206303763706339328-ylh9',
+            github: 'https://github.com/Gabriellealsantos/git-api'
         },
         {
-            "title": "League of Legends | Chat com Campeões - IA",
-            "functionLabel": "Função",
-            "texts": [
-                "Experiência interativa que permite conversas dinâmicas com campeões de League of Legends.",
-                "Integração avançada com API de Inteligência Artificial para respostas imersivas e autênticas.",
-                "Projeto desenvolvido durante a Santander Dev Week 2024, explorando NLP e IA generativa."
+            title: 'League of Legends | Chat com Campeões - IA',
+            functionLabel: 'Chat IA',
+            texts: [
+                'Experiência interativa que permite conversas dinâmicas com campeões de League of Legends.',
+                'Integração avançada com API de Inteligência Artificial para respostas imersivas e autênticas.',
+                'Projeto desenvolvido durante a Santander Dev Week 2024, explorando NLP e IA generativa.'
             ],
-            "image": lolProjectImg,
-            "link": "https://www.linkedin.com/posts/gabriel-leal-santos_durante-a-santander-dev-week-2024-tive-a-activity-7207037345882189824-4UJu"
+            image: lolProjectImg,
+            link: 'https://www.linkedin.com/posts/gabriel-leal-santos_durante-a-santander-dev-week-2024-tive-a-activity-7207037345882189824-4UJu',
+            github: 'https://github.com/Gabriellealsantos/IALol'
         },
         {
             title: 'Delivery | Sistema de Entregas',
-            functionLabel: 'Função',
+            functionLabel: 'Sistema de Entregas',
             texts: [
                 'Projeto de delivery completo, integrando web, mobile e back-end para uma experiência fluida.',
                 'Desenvolvido com Java (Spring Boot), ReactJS (TypeScript) e React Native, garantindo robustez e escalabilidade.',
-                'Utilização do PostgreSQL para armazenamento de dados e Mapbox/Google Maps para geolocalização e rotas',
+                'Utilização do PostgreSQL para armazenamento de dados e Mapbox/Google Maps para geolocalização e rotas.'
             ],
             image: deliveryImg,
-            link: 'https://www.linkedin.com/feed/update/urn:li:activity:7283147378876952577/'
+            link: 'https://www.linkedin.com/feed/update/urn:li:activity:7283147378876952577/',
+            github: 'https://github.com/Gabriellealsantos/Delivery'
+        },
+        {
+            title: 'ClimAgora | Sistema Integrado de Clima e Favoritos',
+            functionLabel: 'Aplicação Clima',
+            texts: [
+                'Aplicativo que integra dados climáticos em tempo real utilizando a API OpenWeather, aliado à API Geocode para a validação e busca de cidades.',
+                'Permite aos usuários pesquisar, visualizar e favoritar cidades, mantendo uma lista personalizada de locais preferidos.',
+                'Implementa autenticação segura e criação de conta com JWT e OAuth2.'
+            ],
+            image: climaImg,
+            link: 'https://www.linkedin.com/posts/gabriel-leal-santos_springboot-java-api-activity-7298370103065108483-HaqM',
+            github: 'https://github.com/Gabriellealsantos/climaAgora'
         }
     ];
+
 
     return (
         <>
@@ -98,6 +117,7 @@ export default function Project() {
                                 title={project.title}
                                 functionLabel={project.functionLabel}
                                 texts={project.texts}
+                                git={project.github}
                             />
 
                             <ImageCard
